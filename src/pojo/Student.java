6a1b8e23f81bd54aa1fdd4bd945fdba730490c92
cyss <2363067648@qq.com>
2021-12-21 19:44:08 +0800

    master commit

--- src/pojo/Student.java
+++ src/pojo/Student.java
@@ -37,7 +37,7 @@ public class Student {
 
     public void page(){
         System.out.println("???");
-        System.out.println("???");
+        System.out.println("hello.txt");
     }
 
     public void page1(){
